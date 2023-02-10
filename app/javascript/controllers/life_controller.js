024ebb1fb36ad7ae31860ceeeddfa4f1c4982a48
Dean DeHart <DeHartDean@gmail.com>
2023-02-10 03:48:55 -0500

    Finish e2

--- app/javascript/controllers/life_controller.js
+++ app/javascript/controllers/life_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 import Cell from "../life/cell";
+import { structures } from "../life/structures";
+import { placementType } from "../life/settings";
 
 // Connects to data-controller="life"
 export default class extends Controller {
@@ -10,7 +12,10 @@ export default class extends Controller {
     this.context = this.canvas.getContext("2d");
     this.width = 72;
     this.height = 48;
+    this.canvas.width = this.width * Cell.width;
+    this.canvas.height = this.height * Cell.height;
 
+    this.placementType = placementType.Cell;
     this.start_button = document.getElementById("start_button");
 
     this.running = false;
@@ -99,11 +104,20 @@ export default class extends Controller {
   }
   toggleCellByButton(x, y, buttons) {
     if (buttons === 1) {
-      this.toggleCell(x, y, true);
+      this.toggleByPlacmentType(x, y);
     } else if (buttons === 2) {
       this.toggleCell(x, y, false);
     }
   }
+
+  toggleByPlacmentType(x, y) {
+    if (this.placementType === placementType.Cell) {
+      this.toggleCell(x, y, true);
+    } else {
+      this.placeStructure(x, y);
+    }
+  }
+
   toggleCell(x, y, alive) {
     this.grid[x][y].alive = alive;
   }
@@ -113,11 +127,7 @@ export default class extends Controller {
   }
   clear(event) {
     this.running = false;
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        this.grid[x][y].alive = false;
-      }
-    }
+    this.initGrid();
     this.toggleStartButtonText();
   }
   random(event) {
@@ -135,6 +145,34 @@ export default class extends Controller {
     }
     if (!this.running) {
       start_button.innerText = "Start";
+    }
+  }
+
+  setPlacementType(event) {
+    this.placementType = event.target.value;
+  }
+
+  placeStructure(x, y) {
+    if (this.placementType === placementType.Glider) {
+      structures.glider.place(this.grid, x, y);
+    }
+    if (this.placementType === placementType.GliderGun) {
+      structures.gliderGun.place(this.grid, x, y);
+    }
+    if (this.placementType === placementType.Ship) {
+      structures.ship.place(this.grid, x, y);
+    }
+    if (this.placementType === placementType.Spaceship) {
+      structures.spaceship.place(this.grid, x, y);
+    }
+    if (this.placementType === placementType.Pulsar) {
+      structures.pulsar.place(this.grid, x, y);
+    }
+    if (this.placementType === placementType.rPentomino) {
+      structures.rPentomino.place(this.grid, x, y);
+    }
+    if (this.placementType === placementType.Replicator) {
+      structures.replicator.place(this.grid, x, y);
     }
   }
 }
